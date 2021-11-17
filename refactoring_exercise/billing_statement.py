397c0591ccd981973e9634f51e745aad9c3b7fb2
Philip Vegdahl <pvegdahl@gmail.com>
2021-11-17 10:46:10 -0500

    Black formatting

---
+++ refactoring_exercise/billing_statement.py
@@ -0,0 +1,57 @@
+import math
+from typing import Dict, NamedTuple, List
+
+
+class Play(NamedTuple):
+    name: str
+    type: str
+
+
+class Performance(NamedTuple):
+    play_id: str
+    audience: int
+
+
+class Invoice(NamedTuple):
+    customer: str
+    performances: List[Performance]
+
+
+def billing_statement(invoice: Invoice, plays: Dict[str, Play]):
+    total_amount = 0
+    volume_credits = 0
+    result = f"Statement for {invoice.customer}\n"
+
+    # TODO? Currency formatter
+
+    for performance in invoice.performances:
+        play = plays[performance.play_id]
+        if play.type == "tragedy":
+            this_amount = 40000
+            if performance.audience > 30:
+                this_amount += 1000 * (performance.audience - 30)
+        elif play.type == "comedy":
+            this_amount = 30000
+            if performance.audience > 20:
+                this_amount += 10000 + 500 * (performance.audience - 30)
+            this_amount += 300 * performance.audience
+        else:
+            raise Exception(f"Unknown play type: {play.type}")
+
+        # Add volume credits
+        volume_credits += max(0, performance.audience - 30)
+        # And extra for every ten comedy attendees
+        if play.type == "comedy":
+            volume_credits += math.floor(performance.audience / 10)
+
+        # Add the line for this order
+        # TODO: use number formatting here
+        result += (
+            f"  {play.name}: ${this_amount / 100} ({performance.audience} seats)\n"
+        )
+        total_amount += this_amount
+
+    result += f"Amount owed is ${total_amount / 100}\n"
+    result += f"You earned {volume_credits} credits\n"
+
+    return result
--- refactoring_exercise/test_billing_statement.py
+++ refactoring_exercise/test_billing_statement.py
@@ -1,8 +1,44 @@
-import unittest
+from refactoring_exercise.billing_statement import (
+    Play,
+    Performance,
+    Invoice,
+    billing_statement,
+)
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)  # add assertion here
+PLAYS = {
+    "glass": Play(name="The Glass Menagerie", type="tragedy"),
+    "noise": Play(name="Noises Off", type="comedy"),
+}
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_tragedy_under_volume_threshold():
+    invoice = Invoice(
+        customer="ElectricCompany",
+        performances=[Performance(play_id="glass", audience=25)],
+    )
+    assert (
+        billing_statement(invoice=invoice, plays=PLAYS)
+        == """Statement for ElectricCompany
+  The Glass Menagerie: $400.0 (25 seats)
+Amount owed is $400.0
+You earned 0 credits
+"""
+    )
+
+
+def test_true():
+    assert True
+
+
+# Tests
+# Tragedy
+#   Less than 30
+#   Exactly 30
+#   More than 30
+# Comedy
+#   Less than 20
+#   Exactly 20
+#   More than 20
+#   More than 30
+# Both
+# Volume credits
