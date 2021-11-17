a3d018cd87e21cba53d7044389175f996896a3fe
Philip Vegdahl <pvegdahl@gmail.com>
2021-11-17 11:29:17 -0500

    Finished the tests

--- refactoring_exercise/test_billing_statement.py
+++ refactoring_exercise/test_billing_statement.py
@@ -55,15 +55,78 @@ You earned 10 credits
 """
     )
 
-def test_true():
-    assert True
+
+def test_comedy_under_volume_threshold():
+    invoice = Invoice(
+        customer="StairMaster",
+        performances=[Performance(play_id="noise", audience=17)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for StairMaster
+  Noises Off: $351.0 (17 seats)
+Amount owed is $351.0
+You earned 1 credits
+"""
+    )
 
 
-# Tests
-# Comedy
-#   Less than 20
-#   Exactly 20
-#   More than 20
-#   More than 30
-# Both
-# Volume credits
+def test_comedy_exactly_volume_threshold():
+    invoice = Invoice(
+        customer="StairMaster",
+        performances=[Performance(play_id="noise", audience=20)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for StairMaster
+  Noises Off: $360.0 (20 seats)
+Amount owed is $360.0
+You earned 2 credits
+"""
+    )
+
+
+def test_comedy_exactly_volume_credit_threshold():
+    invoice = Invoice(
+        customer="StairMaster",
+        performances=[Performance(play_id="noise", audience=30)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for StairMaster
+  Noises Off: $490.0 (30 seats)
+Amount owed is $490.0
+You earned 3 credits
+"""
+    )
+
+
+def test_comedy_above_both_thresholds():
+    invoice = Invoice(
+        customer="StairMaster",
+        performances=[Performance(play_id="noise", audience=47)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for StairMaster
+  Noises Off: $626.0 (47 seats)
+Amount owed is $626.0
+You earned 21 credits
+"""
+    )
+
+
+def test_multiple_performances():
+    invoice = Invoice(
+        customer="BigConglomerate",
+        performances=[Performance(play_id="noise", audience=86), Performance(play_id="glass", audience=99)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for BigConglomerate
+  Noises Off: $938.0 (86 seats)
+  The Glass Menagerie: $1090.0 (99 seats)
+Amount owed is $2028.0
+You earned 133 credits
+"""
+    )
