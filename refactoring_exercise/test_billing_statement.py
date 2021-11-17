00b691d2a177ada8e6e9860a53fc03bd7d51c0ec
Philip Vegdahl <pvegdahl@gmail.com>
2021-11-17 10:49:18 -0500

    Tragedy tests are done

--- refactoring_exercise/test_billing_statement.py
+++ refactoring_exercise/test_billing_statement.py
@@ -26,15 +26,40 @@ You earned 0 credits
     )
 
 
+def test_tragedy_exactly_volume_threshold():
+    invoice = Invoice(
+        customer="ElectricCompany",
+        performances=[Performance(play_id="glass", audience=30)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for ElectricCompany
+  The Glass Menagerie: $400.0 (30 seats)
+Amount owed is $400.0
+You earned 0 credits
+"""
+    )
+
+
+def test_tragedy_over_volume_threshold():
+    invoice = Invoice(
+        customer="ElectricCompany",
+        performances=[Performance(play_id="glass", audience=40)],
+    )
+    assert (
+            billing_statement(invoice=invoice, plays=PLAYS)
+            == """Statement for ElectricCompany
+  The Glass Menagerie: $500.0 (40 seats)
+Amount owed is $500.0
+You earned 10 credits
+"""
+    )
+
 def test_true():
     assert True
 
 
 # Tests
-# Tragedy
-#   Less than 30
-#   Exactly 30
-#   More than 30
 # Comedy
 #   Less than 20
 #   Exactly 20
